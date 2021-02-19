90c25bf95776ba14987b26ea6c4987f9af6f5f13
Ihar Hancharenka <iigoncharenko@gmail.com>
2021-02-19 09:08:28 +0300

    m

--- .sh.d/pipenv.sh
+++ .sh.d/pipenv.sh
@@ -1,7 +1,20 @@
 #export WORKON_HOME="/home/iharh/.venvs"
 export PIPENV_VENV_IN_PROJECT=1
 
-dvc () {
-    (. $WRK_PRJ_DIR/py/dvc/.venv/bin/activate && $WRK_PRJ_DIR/py/dvc/.venv/bin/dvc "$@")
+prj-activate() {
+    local PRJ_NAME="${1// }"
+    shift
+    if [[ -z $PRJ_NAME ]]; then
+        echo "PRJ_NAME is EMPTY - skip activation"
+    else
+        (. $WRK_PRJ_DIR/py/$PRJ_NAME/.venv/bin/activate && $WRK_PRJ_DIR/py/$PRJ_NAME/.venv/bin/$PRJ_NAME "$@")
+    fi
 }
 
+dvc () {
+    prj-activate dvc "$@"
+}
+
+youtube-dl () {
+    prj-activate youtube-dl "$@"
+}
